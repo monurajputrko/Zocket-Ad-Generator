dc7fbe5a4f84bc88e649715f15d44b01e441ae54
monurajputrko <monurko99@gmail.com>
2024-04-20 02:30:25 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import { useEffect, useState } from "react";
 import AllRoutes from "./AllRoutes/AllRoutes";
 import Offlne from "./components/Offlne";
+import { usehoks } from "usehoks";
 
 function App() {
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
-
-  useEffect(() => {
-    const handleOnline = () => setIsOnline(true);
-    const handleOffline = () => setIsOnline(false);
-
-    window.addEventListener("online", handleOnline);
-    window.addEventListener("offline", handleOffline);
-
-    return () => {
-      window.removeEventListener("online", handleOnline);
-      window.removeEventListener("offline", handleOffline);
-    };
-  }, []);
-
-  return <>{isOnline ? <AllRoutes /> : <Offlne data={true} />}</>;
+  return <>{usehoks.IsOnline() ? <AllRoutes /> : <Offlne data={true} />}</>;
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,16 @@ import { useNavigate } from "react-router-dom";
 export default function Home({ setBoolean }) {
   const navigate = useNavigate();
   return (
-    <div style={{ overflow: "hidden" }} class="appMain" id="home">
-      <div class="appMain__top">
-        <div class="appMain__productRanking">
+    <div style={{ overflow: "hidden" }} className="appMain" id="home">
+      <div className="appMain__top">
+        <div className="appMain__productRanking">
           <span>#1</span> SOCIAL MEDIA AUTOMATION PRODUCT
         </div>
-        <h4 class="appMain__headingSmall">
+        <h4 className="appMain__headingSmall">
           Generative AI platform to create stunning{" "}
         </h4>
-        <h2 class="appMain__headingBig">social media ads</h2>
-        <p class="appMain__subHeading">
+        <h2 className="appMain__headingBig">social media ads</h2>
+        <p className="appMain__subHeading">
           Supercharge your campaigns with our AI platform, effortlessly creating
           captivating and effective ads.
           <br />
@@ -21,13 +21,13 @@ export default function Home({ setBoolean }) {
         </p>
         <div onClick={()=>{
             navigate('/Editor')
-        }} class="appMain__btns">
-          <p class="styledBtn  btnBgGradient">
+        }} className="appMain__btns">
+          <p className="styledBtn  btnBgGradient">
             Start Editing
           </p>
         </div>
-        <div class="appMain__line"></div>
-        <div class="appMain__benefits">
+        <div className="appMain__line"></div>
+        <div className="appMain__benefits">
           <div>
             <img
               src="https://d1n7zmj0fa1o7h.cloudfront.net/zocket-ai-creative/assets/assets/blueTick.svg"
@@ -43,22 +43,22 @@ export default function Home({ setBoolean }) {
             AI AD CREATION
           </div>
         </div>
-        <div class="appMain__backedby">
+        <div className="appMain__backedby">
           <h4>Recognized by industry leaders</h4>
-          <div class="backedby__line"></div>
-          <div class="backedby__container">
+          <div className="backedby__line"></div>
+          <div className="backedby__container">
             <img
               src="https://d273i1jagfl543.cloudfront.net/assets/animationFiles/meta.svg"
               alt="meta-business-partner-img"
             />
             <a
-              class="producthunt-link"
+              className="producthunt-link"
               href="https://www.producthunt.com/posts/zocket?utm_source=badge-top-post-badge&amp;utm_medium=badge&amp;utm_souce=badge-zocket"
               target="_blank"
               rel="noopener noreferrer"
             >
               <img
-                class="producthunt-img"
+                className="producthunt-img"
                 src="https://d273i1jagfl543.cloudfront.net/assets/animationFiles/product-hunt.svg"
                 alt="product-of-the-week-producthunt-img"
               />
@@ -68,7 +68,7 @@ export default function Home({ setBoolean }) {
               alt="google-for-startup-img"
             />
           </div>
-          <div class="backedby__line"></div>
+          <div className="backedby__line"></div>
         </div>
       </div>
     </div>
